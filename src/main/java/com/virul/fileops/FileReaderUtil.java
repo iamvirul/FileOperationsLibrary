d2869e1143aa834984b16af57b7319ece90f1522
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-07-17 08:53:36 +0530

    Update FileReaderUtil.java

--- src/main/java/com/virul/fileops/FileReaderUtil.java
+++ src/main/java/com/virul/fileops/FileReaderUtil.java
@@ -4,10 +4,23 @@
  */
 package main.java.com.virul.fileops;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Virul Nirmala Wickramasinghe
  */
 public class FileReaderUtil {
-    
+     public static String readFile(String filePath) throws IOException {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        }
+        return content.toString();
+    }
 }
