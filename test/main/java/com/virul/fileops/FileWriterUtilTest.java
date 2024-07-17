6904f98d89060c82cfcb334294993da4a7b0e077
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-07-17 09:06:00 +0530

    Update FileWriterUtilTest.java

--- test/main/java/com/virul/fileops/FileWriterUtilTest.java
+++ test/main/java/com/virul/fileops/FileWriterUtilTest.java
@@ -4,54 +4,14 @@
  */
 package main.java.com.virul.fileops;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Virul Nirmala Wickramasinghe
  */
 public class FileWriterUtilTest {
-    
-    public FileWriterUtilTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of writeFile method, of class FileWriterUtil.
-     */
-    @Test
-    public void testWriteFile() throws Exception {
-        System.out.println("writeFile");
-        String filePath = "";
-        String content = "";
-        FileWriterUtil.writeFile(filePath, content);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of appendToFile method, of class FileWriterUtil.
-     */
     @Test
     public void testAppendToFile() throws Exception {
         System.out.println("appendToFile");
@@ -59,5 +19,4 @@ public class FileWriterUtilTest {
         String content = "";
         FileWriterUtil.appendToFile(filePath, content);
     }
-    
 }
