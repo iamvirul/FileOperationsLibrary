662b0167a41c53c9fa541b1e784086b498909bb3
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-07-17 08:53:48 +0530

    Update FileWriterUtil.java

--- src/main/java/com/virul/fileops/FileWriterUtil.java
+++ src/main/java/com/virul/fileops/FileWriterUtil.java
@@ -4,10 +4,24 @@
  */
 package main.java.com.virul.fileops;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Virul Nirmala Wickramasinghe
  */
 public class FileWriterUtil {
-    
+    public static void writeFile(String filePath, String content) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            bw.write(content);
+        }
+    }
+
+    public static void appendToFile(String filePath, String content) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, true))) {
+            bw.write(content);
+        }
+    }
 }
