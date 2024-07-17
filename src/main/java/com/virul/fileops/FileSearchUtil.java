ddfeda3f93fb41e8eb8e150a5110d65644b30ad5
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-07-17 08:53:51 +0530

    Update FileSearchUtil.java

--- src/main/java/com/virul/fileops/FileSearchUtil.java
+++ src/main/java/com/virul/fileops/FileSearchUtil.java
@@ -4,10 +4,27 @@
  */
 package main.java.com.virul.fileops;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Virul Nirmala Wickramasinghe
  */
 public class FileSearchUtil {
-    
+       public static List<String> searchInFile(String filePath, String keyword) throws IOException {
+        List<String> matchingLines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains(keyword)) {
+                    matchingLines.add(line);
+                }
+            }
+        }
+        return matchingLines;
+    }
 }
