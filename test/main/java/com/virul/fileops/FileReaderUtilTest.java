a2f9d50719fdb4262886c07f3f89a8750e8aad59
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2024-07-17 09:06:06 +0530

    Update FileReaderUtilTest.java

--- test/main/java/com/virul/fileops/FileReaderUtilTest.java
+++ test/main/java/com/virul/fileops/FileReaderUtilTest.java
@@ -4,10 +4,6 @@
  */
 package main.java.com.virul.fileops;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,29 +12,7 @@ import static org.junit.Assert.*;
  * @author Virul Nirmala Wickramasinghe
  */
 public class FileReaderUtilTest {
-    
-    public FileReaderUtilTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of readFile method, of class FileReaderUtil.
-     */
     @Test
     public void testReadFile() throws Exception {
         System.out.println("readFile");
@@ -47,5 +21,4 @@ public class FileReaderUtilTest {
         String result = FileReaderUtil.readFile(filePath);
         assertEquals(expResult, result);
     }
-    
 }
